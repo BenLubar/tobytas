606b26273640cf3d6b5c55a6f6f37fb3ba9428ba
Ben Lubar <ben.lubar@gmail.com>
2018-11-28 14:49:23 -0600

    Improvements to readout.go:
    - Precompute image masks at the start of the render.
    - Clean up variable naming.
    - Only blank out the background from the first input on.
    - Move inputs to the middle of the screen and wash out their colors to the left.
    
    Also fixes the splash image being shown for 90 frames instead of 180 and
    just happening to work because nothing else wrote to video memory in
    between.
    
    Also add a disclaimer to the splash image.

--- readout.go
+++ readout.go
@@ -222,93 +222,162 @@ xxxxxxxxOxOxx...
 .......xxx......`,
 }
 
+var buttonMasks = [7][2]*image.Alpha{
+	makeMask(buttons[0]),
+	makeMask(buttons[1]),
+	makeMask(buttons[2]),
+	makeMask(buttons[3]),
+	makeMask(buttons[4]),
+	makeMask(buttons[5]),
+	makeMask(buttons[6]),
+}
+
+func makeMask(str string) [2]*image.Alpha {
+	oimg := image.NewAlpha(image.Rect(0, 0, buttonSize, buttonSize))
+	ximg := image.NewAlpha(image.Rect(0, 0, buttonSize, buttonSize))
+
+	i, j := 0, 0
+	for y := 0; y < buttonSize; y++ {
+		for x := 0; x < buttonSize; x++ {
+			switch str[i] {
+			case 'O':
+				oimg.Pix[j] = 255
+			case 'x':
+				ximg.Pix[j] = 255
+			}
+
+			i++
+			j++
+		}
+		i++
+	}
+
+	return [2]*image.Alpha{oimg, ximg}
+}
+
 const (
-	delay   = 0
-	display = 300
+	buttonCount   = 7
+	width, height = 640, 120
 
-	period = 600 // for color; make it a multiple of 6
+	// button size in pixels (square)
+	buttonSize = 16
 
-	merror = 10
+	// pixels per frame moved by inputs.
+	step = 3
+
+	// time in frames to go through the rainbow; must be divisible by 6
+	period = 600
+
+	// glow time in frames
+	glowHold = 10
+
+	// buttons are pressed when they reach this x position
+	target = (width - buttonSize) / 2
+
+	// top of first button
+	top = (height - buttonCount*buttonSize) / 2
+
+	// number of inputs on the screen (in both directions)
+	display = (target + buttonSize + step) / step
 )
 
 func render(bits []uint8) {
-	var (
-		// detect holds by going forward, then draw the symbols backward
-		// this array is necessary
-		backstring [7][merror + display + 1]bool
-		glowlevel  [7][merror + display + 1]int
-	)
-
-	img := image.NewRGBA(image.Rect(0, 0, 640, 120))
-	for absframe := range bits {
-		draw.Draw(img, img.Rect, image.Black, image.ZP, draw.Src)
-		trueframe := absframe - delay
-		for btn := 0; btn < 7; btn++ {
-			for i := -merror; i <= display; i++ {
-				merof := merror + i
-				refframe := trueframe + i
-				if refframe < 0 || refframe >= len(bits) {
-					backstring[btn][merof] = false
-					glowlevel[btn][merof] = 0
-				} else {
-					if bits[refframe]&(1<<uint(btn)) == 0 {
-						backstring[btn][merof] = false
-						if i == -merror {
-							glowlevel[btn][merof] = 0
-						} else {
-							glowlevel[btn][merof] = glowlevel[btn][merof-1] - 1
-						}
-					} else {
-						backstring[btn][merof] = true
-						if i == -merror {
-							glowlevel[btn][merof] = 1
-						} else {
-
-							glowlevel[btn][merof] = 10
-						}
-					}
-				}
-			}
-			for i := display; i >= 0; i-- {
-				merof := merror + i
-				refframe := trueframe + i
-				if refframe >= len(bits) {
-					continue
-				}
-				if backstring[btn][merof] {
-					if backstring[btn][merof-1] {
-						for k := 0; k < 3; k++ {
-							soverlay(img, buttons[btn], image.Pt(10+i*3-k, 4+16*btn), retrievecolor(refframe), color.RGBA{})
-						}
-					} else {
-						soverlay(img, buttons[btn], image.Pt(10+i*3, 4+16*btn), retrievecolor(refframe), color.RGBA{0, 0, 0, 255})
-					}
-				}
-			}
-
-			mh := uint8(255 * glowlevel[btn][merror] / 10)
-			soverlay(img, buttons[btn], image.Pt(10-1, 4+16*btn), color.RGBA{192, 192, 192, 255}, color.RGBA{mh, mh, mh, 255})
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	left := target
+	for _, b := range bits {
+		left += step
+		if b != 0 {
+			break
 		}
+	}
+
+	for trueFrame := range bits {
+		left -= step
+		if left < 0 {
+			left = 0
+		}
+		draw.Draw(img, image.Rect(left, 0, width, height), image.Black, image.ZP, draw.Src)
+
+		for btn := 0; btn < buttonCount; btn++ {
+			var (
+				// detect holds by going forward, then draw the symbols backward
+				backString [display*2 + 1]bool
+				glowLevel  [display*2 + 1]int
+			)
+
+			for i := -display; i <= display; i++ {
+				offset := display + i
+				refFrame := trueFrame + i
+				if refFrame < 0 || refFrame >= len(bits) {
+					backString[offset] = false
+					glowLevel[offset] = 0
+				} else {
+					if bits[refFrame]&(1<<uint(btn)) == 0 {
+						backString[offset] = false
+						if i == -display || glowLevel[offset-1] <= 0 {
+							glowLevel[offset] = 0
+						} else {
+							glowLevel[offset] = glowLevel[offset-1] - 1
+						}
+					} else {
+						backString[offset] = true
+						if i == -display {
+							glowLevel[offset] = 1
+						} else {
+
+							glowLevel[offset] = glowHold
+						}
+					}
+				}
+			}
+
+			for i := display; i >= 0; i-- {
+				offset := display + i
+				if backString[offset] {
+					fg := retrieveColor(trueFrame + i)
+					if backString[offset-1] {
+						for k := 0; k < step; k++ {
+							overlay(img, buttonMasks[btn], image.Pt(target+i*step-k, top+buttonSize*btn), fg, color.RGBA{})
+						}
+					} else {
+						overlay(img, buttonMasks[btn], image.Pt(target+i*step, top+buttonSize*btn), fg, color.RGBA{0, 0, 0, 255})
+					}
+				}
+			}
+			for i := -1; i >= -display; i-- {
+				offset := display + i
+				if backString[offset] {
+					fg := retrieveColor(trueFrame + i)
+					fg.R = fg.R/8 + 16
+					fg.G = fg.G/8 + 16
+					fg.B = fg.B/8 + 16
+					if offset > 0 && backString[offset-1] {
+						for k := 0; k < step; k++ {
+							overlay(img, buttonMasks[btn], image.Pt(target+i*step-k, top+buttonSize*btn), fg, color.RGBA{})
+						}
+					} else {
+						overlay(img, buttonMasks[btn], image.Pt(target+i*step, top+buttonSize*btn), fg, color.RGBA{0, 0, 0, 255})
+					}
+				}
+			}
+
+			mh := uint8(255 * glowLevel[display] / glowHold)
+			overlay(img, buttonMasks[btn], image.Pt(target-1, top+buttonSize*btn), color.RGBA{192, 192, 192, 255}, color.RGBA{mh, mh, mh, 255})
+		}
+
 		_, err := os.Stdout.Write(img.Pix)
 		check(err)
 	}
 }
 
-func soverlay(img *image.RGBA, str string, pt image.Point, col, ocol color.RGBA) {
-	for x := 0; x < 16; x++ {
-		for y := 0; y < 16; y++ {
-			chr := str[x+y*17]
-			if chr == 'O' && col.A != 0 {
-				img.SetRGBA(pt.X+x, pt.Y+y, col)
-			} else if chr == 'x' && ocol.A != 0 {
-				img.SetRGBA(pt.X+x, pt.Y+y, ocol)
-			}
-		}
-	}
+func overlay(img *image.RGBA, mask [2]*image.Alpha, pt image.Point, col, xcol color.RGBA) {
+	rect := image.Rect(0, 0, buttonSize, buttonSize).Add(pt)
+	draw.DrawMask(img, rect, image.NewUniform(col), image.ZP, mask[0], image.ZP, draw.Over)
+	draw.DrawMask(img, rect, image.NewUniform(xcol), image.ZP, mask[1], image.ZP, draw.Over)
 }
 
-func retrievecolor(refframe int) color.RGBA {
-	phase := refframe % period
+func retrieveColor(refFrame int) color.RGBA {
+	phase := refFrame % period
 	if phase < 0 {
 		phase += period
 	}
