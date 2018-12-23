897ff69901769eeaa0c149855c4562a4c4f4d564
Ben Lubar <ben.lubar@gmail.com>
2018-12-22 18:00:43 -0600

    Set TAS segment as an environment variable in the proxy.

--- proxy.c
+++ proxy.c
@@ -6,7 +6,29 @@
 
 int main(int argc, char *argv[])
 {
+	const char *savedir = getenv("HOME");
+
+	(void)mkdir(savedir, 0755);
+
+	unsigned char segmentNumber = 0;
+	char segment[4];
+
 	char buf[PATH_MAX];
+	snprintf(buf, PATH_MAX, "%s/.tas-segment", savedir);
+	FILE *segmentFile = fopen(buf, "rb");
+	if (segmentFile)
+	{
+		fread(&segmentNumber, 1, 1, segmentFile);
+		fclose(segmentFile);
+	}
+	segmentNumber++;
+	segmentFile = fopen(buf, "wb");
+	fwrite(&segmentNumber, 1, 1, segmentFile);
+	fclose(segmentFile);
+
+	snprintf(segment, 4, "%d", segmentNumber);
+	setenv("TAS_SEGMENT", segment, 1);
+
 	snprintf(buf, PATH_MAX, "%s/.config/DELTARUNE", getenv("HOME"));
 
 	struct stat st;
