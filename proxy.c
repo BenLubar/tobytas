795b53ce89c81679d52649f6ae521dcbabe96904
Ben Lubar <ben.lubar@gmail.com>
2019-02-19 17:47:40 -0600

    Improve handling of save files by setting the HOME directory.

--- proxy.c
+++ proxy.c
@@ -3,12 +3,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[])
 {
 	/* Force time zone to avoid having the TAS desync due to time zone. */
 	setenv("TZ", "America/Chicago", 1);
+
+	/* Set the home directory to force save files to stay separate. */
+	char fakehome[PATH_MAX];
+	getcwd(fakehome, sizeof(fakehome));
+	strcat(fakehome, "/.home");
+	setenv("HOME", fakehome, 1);
 
 	/* Deltarune only has one segment. If we are not on frame 0, start Undertale. */
 	if (strcmp(getenv("LIBTAS_START_FRAME"), "0") != 0)
@@ -19,6 +26,20 @@ int main(int argc, char *argv[])
 		}
 
 		argv[1] = "./undertale-runner";
+	}
+	else
+	{
+		/* If we are on frame 0, delete the old fake home directory first. */
+		pid_t pid = fork();
+		if (pid)
+		{
+			int status;
+			waitpid(pid, &status, 0);
+		}
+		else
+		{
+			execlp("rm", "-rf", fakehome, NULL);
+		}
 	}
 
 	execv(argv[1], argv + 1);
